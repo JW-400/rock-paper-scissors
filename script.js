78adc9c7354aa38442b4aa86847a6e2309f0dcfb
JW-400 <julian.wasmeier@outlook.com>
2022-08-19 03:22:36 +0200

    feat: add winner message at end

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+let compScore = 0;
+let playerScore = 0;
 
 const getComputerChoice = () => {
   const choice = choices[Math.floor(Math.random() * 2) + 1];
@@ -13,14 +15,18 @@ const playRound = (playerChoice, computerChoice) => {
     (computerChoice === "paper" && playerChoiceCaseInsensitive === "rock") ||
     (computerChoice === "scissors" && playerChoiceCaseInsensitive === "paper")
   ) {
+    compScore += 1;
     return `You lose ${computerChoice} beats ${playerChoiceCaseInsensitive}`;
   } else if (playerChoiceCaseInsensitive === computerChoice) {
+    compScore += 1;
+    playerScore += 1;
     return "Tie.";
   } else if (
     (playerChoiceCaseInsensitive === "rock" && computerChoice === "scissors") ||
     (playerChoiceCaseInsensitive === "paper" && computerChoice === "rock") ||
     (playerChoiceCaseInsensitive === "scissors" && computerChoice === "paper")
   ) {
+    playerScore += 1;
     return `You win. ${playerChoiceCaseInsensitive} beats ${computerChoice}`;
   } else {
     return "enter correct shit";
@@ -28,11 +34,21 @@ const playRound = (playerChoice, computerChoice) => {
 };
 
 const game = () => {
+  compScore = 0;
+  playerScore = 0;
+  let message = "";
   for (let i = 0; i < 5; i++) {
     const playerChoice = prompt();
-    console.log(playRound(playerChoice, getComputerChoice()));
     alert(playRound(playerChoice, getComputerChoice()));
   }
+  if (compScore > playerScore) {
+    message = `Computer wins ${compScore} to ${playerScore}`;
+  } else if (compScore === playerScore) {
+    message = `Tie ${compScore} to ${playerScore}`;
+  } else if (compScore < playerScore) {
+    message = `Player wins ${playerScore} to ${compScore}`;
+  }
+  alert(message);
 };
 
 game();
